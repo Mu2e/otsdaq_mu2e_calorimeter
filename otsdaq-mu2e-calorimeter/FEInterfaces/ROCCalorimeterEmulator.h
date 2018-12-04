c9c33065a39b3eadaf6a64ee600efb923e3c97e7
Ryan Rivera <rrivera@fnal.gov>
2018-12-04 10:59:43 -0600

    Added script create_ots_repo.sh to otsdaq_utilities, which creates a repo copy, useful for new repositories. And used it to create the calorimeter emulator repository.

--- otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterEmulator.h
+++ otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterEmulator.h
@@ -1,32 +1,55 @@
 #ifndef _ots_ROCCalorimeterEmulator_h_
 #define _ots_ROCCalorimeterEmulator_h_
 
-#include "otsdaq-mu2e/FEInterfaces/ROCInterface.h"
+#include "otsdaq-mu2e/FEInterfaces/ROCCoreVEmulator.h"
 
 
 namespace ots
 {
-	
 
-class ROCCalorimeterEmulator : public ROCInterface
+
+class ROCCalorimeterEmulator : public ROCCoreVEmulator
 {
-  
- public:
-  //ROCCalorimeterEmulator (const unsigned int linkID, DTCLib::DTC* thisDTC, const unsigned int delay, const ConfigurationTree& theXDAQContextConfigTree, const std::string& theConfigurationPath);
-  
-  //~ROCCalorimeterEmulator(void);
-	
 
-	void writeRegister(unsigned address, unsigned data_to_write);
+public:
+	ROCCalorimeterEmulator (const std::string& rocUID,
+			const ConfigurationTree& theXDAQContextConfigTree,
+			const std::string& interfaceConfigurationPath);
 
-	int readRegister(unsigned address);
-	
+	~ROCCalorimeterEmulator(void);
+
+	void 					writeRegister		(unsigned address, unsigned data_to_write);
+
+	int 					readRegister		(unsigned address);
+
+	bool 					emulatorWorkLoop	(void) override;
+
 	enum{
 		ADDRESS_FIRMWARE_VERSION = 5,
 		ADDRESS_MYREGISTER = 0x65,
 	};
-    int temperature[5];
-}
+
+	//	temperature--
+	class Thermometer
+	{
+	private:
+		double mnoiseTemp;
+	public:
+		void noiseTemp(double intemp)
+		{
+			mnoiseTemp = (double) intemp + 0.05*(intemp*((double) rand() / (RAND_MAX))-0.5);
+			return;
+		}
+		double GetBoardTempC()
+		{
+			return mnoiseTemp;
+		}
+	};
+
+
+	Thermometer 	temp1_;
+	double 			inputTemp_;
+
 };
 
 }
