1c35899171e9c094252f109ba953b589832b8449
Ryan Rivera <rrivera@fnal.gov>
2019-02-19 11:14:27 -0600

    Transformation from 'configuration' to 'table' and new clang format.

--- otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterEmulator.h
+++ otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterEmulator.h
@@ -3,54 +3,48 @@
 
 #include "otsdaq-mu2e/FEInterfaces/ROCCoreVEmulator.h"
 
-
 namespace ots
 {
-
-
 class ROCCalorimeterEmulator : public ROCCoreVEmulator
 {
-
-public:
-	ROCCalorimeterEmulator (const std::string& rocUID,
-			const ConfigurationTree& theXDAQContextConfigTree,
-			const std::string& interfaceConfigurationPath);
+  public:
+	ROCCalorimeterEmulator(const std::string&       rocUID,
+	                       const ConfigurationTree& theXDAQContextConfigTree,
+	                       const std::string&       interfaceConfigurationPath);
 
 	~ROCCalorimeterEmulator(void);
 
-	void 					writeRegister		(unsigned address, unsigned data_to_write) override;
-	int 					readRegister		(unsigned address) override;
+	void writeRegister(unsigned address, unsigned data_to_write) override;
+	int  readRegister(unsigned address) override;
 
-	bool 					emulatorWorkLoop	(void) override;
+	bool emulatorWorkLoop(void) override;
 
-	enum{
+	enum
+	{
 		ADDRESS_FIRMWARE_VERSION = 5,
-		ADDRESS_MYREGISTER = 0x65,
+		ADDRESS_MYREGISTER       = 0x65,
 	};
 
 	//	temperature--
 	class Thermometer
 	{
-	private:
+	  private:
 		double mnoiseTemp;
-	public:
+
+	  public:
 		void noiseTemp(double intemp)
 		{
-			mnoiseTemp = (double) intemp + 0.05*(intemp*((double) rand() / (RAND_MAX))-0.5);
+			mnoiseTemp =
+			    (double)intemp + 0.05 * (intemp * ((double)rand() / (RAND_MAX)) - 0.5);
 			return;
 		}
-		double GetBoardTempC()
-		{
-			return mnoiseTemp;
-		}
+		double GetBoardTempC() { return mnoiseTemp; }
 	};
 
-
-	Thermometer 	temp1_;
-	double 			inputTemp_;
-
+	Thermometer temp1_;
+	double      inputTemp_;
 };
 
-}
+}  // namespace ots
 
 #endif
--- otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterEmulator_interface.cc
+++ otsdaq-mu2e-calorimeter/FEInterfaces/ROCCalorimeterEmulator_interface.cc
@@ -4,16 +4,15 @@
 
 using namespace ots;
 
-#undef 	__MF_SUBJECT__
+#undef __MF_SUBJECT__
 #define __MF_SUBJECT__ "FE-ROCCalorimeterEmulator"
-
 
 //=========================================================================================
 ROCCalorimeterEmulator::ROCCalorimeterEmulator(
-			   const std::string& rocUID,
-			   const ConfigurationTree& theXDAQContextConfigTree,
-			   const std::string& theConfigurationPath)
-: ROCCoreVEmulator			(rocUID,theXDAQContextConfigTree,theConfigurationPath)
+    const std::string&       rocUID,
+    const ConfigurationTree& theXDAQContextConfigTree,
+    const std::string&       theConfigurationPath)
+    : ROCCoreVEmulator(rocUID, theXDAQContextConfigTree, theConfigurationPath)
 {
 	INIT_MF("ROCCalorimeterEmulator");
 
@@ -32,17 +31,14 @@ ROCCalorimeterEmulator::ROCCalorimeterEmulator(
 	temp1_.noiseTemp(inputTemp_);
 }
 
-
 //==========================================================================================
-ROCCalorimeterEmulator::~ROCCalorimeterEmulator(void)
-{
-}
+ROCCalorimeterEmulator::~ROCCalorimeterEmulator(void) {}
 
 //==================================================================================================
 void ROCCalorimeterEmulator::writeRegister(unsigned address, unsigned data_to_write)
 {
-	//lockout member variables for the remainder of the scope
-	//this guarantees the emulator thread can safely access the members
+	// lockout member variables for the remainder of the scope
+	// this guarantees the emulator thread can safely access the members
 	//	Note: the ROCCoreVEmulator locks before calling emulatorWorkLoop
 	std::lock_guard<std::mutex> lock(workloopMutex_);
 
@@ -50,55 +46,52 @@ void ROCCalorimeterEmulator::writeRegister(unsigned address, unsigned data_to_wr
 
 	return;
 
-} //end writeRegister()
+}  // end writeRegister()
 
 //==================================================================================================
 int ROCCalorimeterEmulator::readRegister(unsigned address)
 {
-	//lockout member variables for the remainder of the scope
-	//this guarantees the emulator thread can safely access the members
+	// lockout member variables for the remainder of the scope
+	// this guarantees the emulator thread can safely access the members
 	//	Note: the ROCCoreVEmulator locks before calling emulatorWorkLoop
 	std::lock_guard<std::mutex> lock(workloopMutex_);
 
 	__CFG_COUT__ << "emulator read" << __E__;
 
-	if (address == ADDRESS_FIRMWARE_VERSION)
+	if(address == ADDRESS_FIRMWARE_VERSION)
 		return 0x5;
-	else if (address == ADDRESS_MYREGISTER)
+	else if(address == ADDRESS_MYREGISTER)
 		return temp1_.GetBoardTempC();
 	else
 		return -1;
 
-} //end readRegister()
+}  // end readRegister()
 
 //==================================================================================================
 // return false to stop workloop thread
 bool ROCCalorimeterEmulator::emulatorWorkLoop(void)
-{	
+{
 	__CFG_COUT__ << "emulator working..." << __E__;
 
 	temp1_.noiseTemp(inputTemp_);
 
-
 	usleep(1000000 /*microseconds*/);
-	return true; //true to keep workloop going
+	return true;  // true to keep workloop going
 
-//	float input, inputTemp;
-//	int addBoard, a;
-//	//
-//	addBoard = 105;
-//	inputTemp = 15.;
-//	a = 0;
-//	while( a < 20 ) {
-//		temp1.noiseTemp(inputTemp);
-//		temperature = temp1.GetBoardTempC();
-//		a++;
-//		return temperature;
-//		usleep(1000000);
-//		return true;
-//	}
-} //end emulatorWorkLoop()
-
+	//	float input, inputTemp;
+	//	int addBoard, a;
+	//	//
+	//	addBoard = 105;
+	//	inputTemp = 15.;
+	//	a = 0;
+	//	while( a < 20 ) {
+	//		temp1.noiseTemp(inputTemp);
+	//		temperature = temp1.GetBoardTempC();
+	//		a++;
+	//		return temperature;
+	//		usleep(1000000);
+	//		return true;
+	//	}
+}  // end emulatorWorkLoop()
 
 DEFINE_OTS_INTERFACE(ROCCalorimeterEmulator)
-
